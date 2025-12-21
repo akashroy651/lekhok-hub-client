2bdc35ac7c570f1dffa9fb529f388f44177bd60b
Akash Roy <akashroy65123@gmail.com>
2025-12-21 08:23:32 +0600

    added new update

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -8,11 +8,11 @@ const axiosSecure =axios.create({
 })
 
 const useAxiosSecure = () => {
-const { user,
-     logOut
+const { user
+    //  logOut
      } = useAuth();
 
-const navigate = useNavigate();
+// const navigate = useNavigate();
 
 // JWT Token create // interceptor request 
 useEffect(() => {
@@ -27,18 +27,21 @@ useEffect(() => {
     },(error)=>{
         console.log(error);
 
-        //>
-        const statusCode = error.status;
-        if(statusCode === 401 || statusCode === 403 ){
-            logOut()
-            .then(() => {
-                navigate('/login')
-            })
-        }
+        // //>
+        // const statusCode = error.status:
+        // if(statusCode === 401 || statusCode === 403 ){
+        //     logOut()
+        //     .then(() => {
+        //         navigate('/login')
+        //     })
+        // }
 
-        //<
+        // //<
+
+
 
         return Promise.reject(error);
+
 
     })
 
